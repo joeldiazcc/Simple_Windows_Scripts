aa15cbd73fe3f0d777ae5c683401213debea3fa5
Joel Felipe Díaz Carissimi <64970845+joeldiazcc@users.noreply.github.com>
2022-02-16 22:32:11 +0100

    Create Delete_Temporary-files.bat

---
+++ Delete_Temporary-files.bat
@@ -0,0 +1,16 @@
+:: @author: joeldiazcc
+:: This program just deletes all useless temporary files 
+RD /S /Q %temp%
+MKDIR %temp%
+takeown /f "%temp%" /r /d y
+takeown /f "C:\Windows\Temp" /r /d y
+RD /S /Q C:\Windows\Temp
+MKDIR C:\Windows\Temp
+
+DEL /S /Q "%TMP%\*.*"
+DEL /S /Q "%TEMP%\*.*"
+DEL /S /Q "%WINDIR%\Temp\*.*"
+DEL /S /Q "%USERPROFILE%\Local Settings\Temp\*.*"
+DEL /S /Q "%LOCALAPPDATA%\Temp\*.*"
+
+PowerShell Clear-RecycleBin -Confirm:$false
